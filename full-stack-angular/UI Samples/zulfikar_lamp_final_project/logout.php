dfb38ccb9f3f5b4c340a4329499b993dcfacf889
skrish87 <skrish87@gmail.com>
2020-04-09 13:40:49 -0700

    Restructuring..

--- full-stack-angular/UI Samples/zulfikar_lamp_final_project/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	if (isset($_COOKIE['uid']) && $_COOKIE['uid']) {
-		setcookie('uid', "", time() - 86400, "/");
-		header("Location:login.php");
-	}
-?>
--- full-stack-angular/src/app/app.module.ts
+++ full-stack-angular/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ChatService } from './chat-service';
+import { WeatherService } from './weather-service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [ChatService, WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
